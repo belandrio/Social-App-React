4757a899f32d6eecb8cfc0da4f9b635a0cc9ac41
bsinoimeri <b.sinoimeri@hotmail.com>
2020-08-11 22:15:32 +0200

    Added the function /post for login

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,11 +22,11 @@ class home extends Component {
 
   render() {
     let recentPostsMarkup = this.state.posts ? (
-      this.state.posts.map((post) => <Post post={post} />)
+      this.state.posts.map((post) => <Post key={post.postId} post={post} />)
     ) : (
       <p>Loading...</p>
     );
-    
+
     return (
       <Grid container spacing={10}>
         <Grid item sm={8} xs={12}>
