3dfd6810fed4af9789201f64a884ee5cb72d0af7
bsinoimeri <b.sinoimeri@hotmail.com>
2020-08-18 18:43:45 +0200

    Upload the image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,14 +9,18 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
 
 //Redux
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
 const styles = (theme) => ({
   paper: {
@@ -29,7 +33,7 @@ const styles = (theme) => ({
       '& button': {
         position: 'absolute',
         top: '80%',
-        left: '70%'
+        left: '70%',
       }
     },
     '& .profile-image': {
@@ -61,17 +65,36 @@ const styles = (theme) => ({
   buttons: {
     textAlign: 'center',
     '& a': {
-      margin: '20px 10px'
+      margin: '20px 10px',
     }
   }
 });
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  };
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  };
+
   render() {
     const {
       classes,
       user: {
-        credentials: { userHandle, createdAt, imageUrl, bio, website, location },
+        credentials: {
+          userHandle,
+          createdAt,
+          imageUrl,
+          bio,
+          website,
+          location,
+        },
         loading,
         authenticated
       }
@@ -83,6 +106,17 @@ class Profile extends Component {
           <div className={classes.profile}>
             <div className="image-wrapper">
               <img src={imageUrl} alt="profile" className="profile-image" />
+              <input
+                type='file'
+                id='imageInput'
+                hidden='hidden'
+                onChange={this.handleImageChange}
+              />
+              <Tooltip title='Edit profile picture' placement='top'>
+                <IconButton onClick={this.handleEditPicture} className='button'>
+                  <EditIcon color='primary' />
+                </IconButton>
+              </Tooltip>
             </div>
             <hr />
             <div className="profile-details">
@@ -151,13 +185,20 @@ class Profile extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  user: state.user
-});
-
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapActionsToProps = { logoutUser, uploadImage };
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Profile));
